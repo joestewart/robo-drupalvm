8c7ac676af18776119600aaa568cce45d6a6ea23
Joe Stewart <joe@joestewartjr.com>
2016-06-01 06:22:00 -0500

    Add initial test setup with Travis

---
+++ RoboFile.php
@@ -0,0 +1,17 @@
+<?php
+class RoboFile extends \Robo\Tasks
+{
+
+    public function test()
+    {
+        $this->stopOnFail(true);
+        $this->taskPHPUnit()
+            ->option('disallow-test-output')
+            ->option('report-useless-tests')
+            ->option('strict-coverage')
+            ->option('-v')
+            ->option('-d error_reporting=-1')
+            ->arg('tests')
+            ->run();
+    }
+}
