4a487f3ebb8196861b89e4633dd06351ef948049
Joe Stewart <joe@joestewartjr.com>
2016-06-17 06:33:08 -0500

    Read config.yml

--- src/Base.php
+++ src/Base.php
@@ -18,6 +18,20 @@ abstract class Base extends \Robo\Tasks implements TaskInterface
     private $vendor_dir;
     private $vendor_bin;
 
+    /**
+     * @var array
+     */
+    public $configuration = array();
+
+    public function __construct() {
+
+
+        if (file_exists($this->getVagrantConfig())) {
+          $contents = file_get_contents($this->getVagrantConfig());
+          $this->configuration = \Symfony\Component\Yaml\Yaml::parse($contents);
+        }
+    }
+
     protected function getProjectRoot($project_root =  __DIR__ . '/../../../../') {
       return realpath($project_root);
     }
@@ -59,6 +73,15 @@ abstract class Base extends \Robo\Tasks implements TaskInterface
           ->run()
           ->getMessage();
       return str_replace("\n", '', $value);
+    }
+
+    protected function getVagrantConfig($config_file = 'config/config.yml') {
+        return $this->getProjectRoot() . '/' . $config_file;
+    }
+
+    public function getConfigValue($variable_name) {
+        $variable_value = $this->configuration[$variable_name];
+        return $variable_value;
     }
 }
 
