74a90a49890dc430dbecc89d2100d87d6b315f26
Joe Stewart <joe@joestewartjr.com>
2016-06-21 10:54:55 -0500

    Update README.md

--- RoboFile.php
+++ RoboFile.php
@@ -16,16 +16,4 @@ class RoboFile extends \Robo\Tasks
       $this->configuration = $this->vm->configuration;
     }
 
-    public function vmTest()
-    {
-        $this->stopOnFail(true);
-        $this->taskPHPUnit()
-            ->option('disallow-test-output')
-            ->option('report-useless-tests')
-            ->option('strict-coverage')
-            ->option('-v')
-            ->option('-d error_reporting=-1')
-            ->arg('tests')
-            ->run();
-    }
 }
