66cbec0dd7a600e4f3bb27379af8c0d0bee6d45b
Joe Stewart <joe@joestewartjr.com>
2016-06-22 07:11:19 -0500

    Remove vm:test command

--- RoboFile.php
+++ RoboFile.php
@@ -15,4 +15,17 @@ class RoboFile extends \Robo\Tasks
       $this->configuration = $this->vm->configuration;
     }
 
+    public function test()
+    {
+        $this->stopOnFail(true);
+        $this->taskPHPUnit()
+            ->option('disallow-test-output')
+            ->option('report-useless-tests')
+            ->option('strict-coverage')
+            ->option('-v')
+            ->option('-d error_reporting=-1')
+            ->arg('tests')
+            ->run();
+    }
+
 }
--- src/Command/Vm.php
+++ src/Command/Vm.php
@@ -18,18 +18,4 @@ trait Vm
             ->run();
         return $result;
     }
-
-    public function vmTest()
-    {
-        $this->stopOnFail(true);
-        $this->taskPHPUnit()
-            ->option('disallow-test-output')
-            ->option('report-useless-tests')
-            ->option('strict-coverage')
-            ->option('-v')
-            ->option('-d error_reporting=-1')
-            ->arg('tests')
-            ->run();
-    }
-
 }
