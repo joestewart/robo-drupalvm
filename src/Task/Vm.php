66f9be44e45619f1895a930e1055e0e78cf8e645
Joe Stewart <joe@joestewartjr.com>
2016-07-06 06:07:12 -0500

    Remove unused use statements since extending Base Task

--- src/Task/Vm.php
+++ src/Task/Vm.php
@@ -4,12 +4,6 @@ namespace JoeStewart\RoboDrupalVM\Task;
 
 
 use Robo\Result;
-use Robo\Task\BaseTask;
-use Robo\Common\ExecOneCommand;
-use Robo\Contract\CommandInterface;
-use Robo\Contract\TaskInterface;
-use Robo\Contract\PrintedInterface;
-use Robo\Exception\TaskException;
 
 class Vm extends \JoeStewart\RoboDrupalVM\Task\Base
 {
